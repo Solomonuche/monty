69086e28ba973742407bd11c299a366bcfc8713e
Solomon <solomonuche@gmail.com>
2023-09-22 02:20:25 +0100

    pint operation

--- pint.c
+++ pint.c
@@ -15,6 +15,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free_gvar();
 		exit(EXIT_FAILURE);
 	}
 
