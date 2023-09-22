57bdcbf5eefa6e307213b982bc16c19441ef3a3b
Solomon <solomonuche@gmail.com>
2023-09-22 03:27:20 +0100

    do nothing

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * nop - do nothing
+ * @stack: linked list
+ * @line_number: line number
+ *
+ * Return: (nothing)
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
