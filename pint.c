639f00cfb9984ea892b7b4a77f74c387bd64f56f
Solomon <solomonuche@gmail.com>
2023-09-22 02:08:00 +0100

    pint operation

---
+++ pint.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ * pint - a function that displays top contents of a stack
+ * @stack: linked list
+ * @line_number: line number
+ *
+ * Return: (nothing)
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+
+	if (*stack == NULL)
+	{
+		printf("L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+
+	while (temp->next != 0)
+	{
+		temp = temp->next;
+	}
+
+	printf("%d\n", temp->n);
+}
